4ad05cd6de96e07f45ee45dbd053209d6bac5d40
Jim Wang <92169701+jim-mode@users.noreply.github.com>
2021-12-10 11:11:36 -0600

    Feature/setup ci pipeline (#1)
    setting up files

---
+++ hello_test.py
@@ -0,0 +1,24 @@
+import allure
+import pytest
+
+@allure.feature('Feature1')
+@allure.story('Story1')
+def test_capitalize_string_01():
+    assert capitalize_string('test') == 'Test'
+
+@allure.feature('Feature2')
+@allure.story('Story2', 'Story3')
+@allure.story('Story4')
+class TestCap:
+    def test_capitalize_string_02(self):
+        assert capitalize_string('python') == 'Python'
+
+@allure.feature('Feature3')
+@pytest.mark.fruits
+def test_capitalize_string_03():
+    assert capitalize_string('apple') != 'Pear'
+
+def capitalize_string(s):
+  if not isinstance(s, str):
+    raise TypeError('Please provide a string')
+  return s.capitalize()
